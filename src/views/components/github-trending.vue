00bacb378efe3debf7ea28a2ff94000fac9acf8a
卓文理 <531840344@qq.com>
2018-01-23 17:06:57 +0800

    :lipstick: 调整样式

--- src/views/components/github-trending.vue
+++ src/views/components/github-trending.vue
@@ -23,7 +23,7 @@
 
         .github-trending__content(v-loading="loading")
             waterfall(
-                :line-gap="340"
+                :line-gap="320"
                 :max-line-gap="360"
                 :min-line-gap="300"
                 :single-max-width="400"
@@ -34,8 +34,8 @@
                 waterfall-slot(
                     v-for="item in trendings"
                     :key="item.repo"
-                    :width="340"
-                    :height="230"
+                    :width="320"
+                    :height="224"
                 )
                     el-card.trending(@click.native="onLinkTapAction(item)")
                         .trending__title {{item.repo}}
