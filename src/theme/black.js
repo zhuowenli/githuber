82b6dd9d1b9b96b399f57b529e62f85640bf3637
hyc <790864964@qq.com>
2018-11-21 09:48:17 +0800

    黑色主题配色

--- src/theme/black.js
+++ src/theme/black.js
@@ -2,12 +2,16 @@
 'use strict';
 
 export default {
-    '--fontColor': '#babbbb',
-    '--background': '#323335',
+    '--fontColor': '#ffffff',
+    '--background': '#282828',
+    '--backgroundLighter': '#232222',
+    '--backgroundLightest': '#000000',
+    '--borderColor': '#252323',
+    '--shadowColor': '#191414',
     '--green': '#67C23A',
     '--orange': '#E6A23C',
     '--red': '#F56C6C',
     '--blue': '#409EFF',
-    '--gray': '#909399',
+    '--gray': '#8c8c8c',
     '--darkBlue': '#4285f4',
 };
--- src/theme/default.js
+++ src/theme/default.js
@@ -4,6 +4,10 @@
 export default {
     '--fontColor': '#303133',
     '--background': '#ffffff',
+    '--backgroundLighter': '#eaeaea',
+    '--backgroundLightest': '#c0c4cc',
+    '--borderColor': '#ebeef5',
+    '--shadowColor': '#dddddd',
     '--green': '#67C23A',
     '--orange': '#E6A23C',
     '--red': '#F56C6C',
